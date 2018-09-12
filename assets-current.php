5f1c112a17a3fc3afcd3b591b1c1685a90d2046b
Abdul Mubeen <abdulmubeenkhan00@gmail.com>
2018-09-12 12:15:28 +0500

    assets updated

---
+++ assets-current.php
@@ -0,0 +1,119 @@
+<?php
+include 'includes/header.php' ?>
+
+
+
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+     
+<!--      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>-->
+    </section>
+    <hr>
+    <!-- Main content -->
+    <section class="content">
+        
+        
+        
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        
+          
+         
+          
+     
+        
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+     <div class="row">
+         <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+         
+          <!-- /.box -->
+
+          <div class="box">
+               <div class="box-header" style="padding:50px 20px;">
+               <a href="add-new-customer.php" class="btn btn-default">Copy</a>
+               <a href="add-new-customer.php" class="btn btn-default">CSV</a>
+               <a href="add-new-customer.php" class="btn btn-default">Excel</a>
+               <a href="add-new-customer.php" class="btn btn-default">PDF</a>
+               <a href="add-new-customer.php" class="btn btn-default">Print</a>
+               
+               
+            </div>
+              
+            <div class="box-header">
+              <h3 class="box-title">Current</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+	       
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Account Type</th>
+                    <th>Balance</th>
+                    <th>Actions</th>
+                  
+                  
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td>Desc</td>
+                        <td>Type</td>
+                        <td>Balance</td>
+                        <td>
+                            <a href=""><i class="fa fa-edit"></i></a>
+                            <a href=""><i class="fa fa-trash"></i></a>
+                        </td>
+                    </tr>
+	    
+                
+                
+                </tbody>
+                
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          
+          
+         
+          
+          
+          
+          
+          
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+      </div>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php' ?>
