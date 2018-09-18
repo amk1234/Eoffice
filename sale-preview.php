3bca8d4a6f85f48545d8afe648c7003172912f17
Abdul Mubeen <abdulmubeenkhan00@gmail.com>
2018-09-18 09:28:18 +0500

    new updates

---
+++ sale-preview.php
@@ -0,0 +1,192 @@
+<?php
+include 'includes/header.php' ?>
+
+
+
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      
+     
+    </section>
+    <hr>
+    <!-- Main content -->
+    <section class="content">
+        <h3>eoffice Manager</h3>
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-default">
+            <div class="inner">
+             
+              <p>Billing Address</p>
+              <p><b>Lawrence D Benally</b></p>
+              <p>12999 NW Blackberry Ln, Seal Rock, OR, 97376</p>
+              <p>Phone: 478-501-0328</p>
+              <p>Email: larrance@email.com</p>
+            </div>
+           
+           
+          </div>
+        </div>
+          
+        <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-default">
+            <div class="inner">
+             
+              <p>Shipping Address</p>
+              <p><b>Lawrence D Benally</b></p>
+              <p>12999 NW Blackberry Ln, Seal Rock, OR, 97376</p>
+              <p>Phone: 478-501-0328</p>
+              <p>Email: larrance@email.com</p>
+            </div>
+           
+           
+          </div>
+        </div>
+          
+         <div class="col-lg-4 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-default">
+            <div class="inner">
+             
+              <h3>Invoice # 1039</h3>
+              <p>Order Date: 17 Sep, 2018</p>
+              <p>Payment Due: 17 Sep, 2018</p>
+             
+            </div>
+           
+           
+          </div>
+        </div>
+       
+     
+      </div>
+      <!-- /.row -->
+      <!-- Main row -->
+      <div class="row">
+         <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+         
+          <!-- /.box -->
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">My Recent Listings</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table  class="table table-bordered table-striped">
+                <thead>
+                <tr>
+	       <th><input type="checkbox"></th>
+               <th>SI</th>
+               <th>Product</th>
+               <th>Description</th>
+               <th>Price</th>
+               <th>Qty</th>
+               <th>Sub Total</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+	       <td><input type="checkbox"></td>  
+                  <td>1</td>
+                  <td>Res. Plot 4500 Sq. Ft. </td>
+                  <td>LDA Avenue Block D</td>
+                  <td>6,700,000</td>
+                  <td>4,500</td>
+                  <td>For Sale</td>
+                 
+                </tr>
+	   
+                
+                
+                </tbody>
+                
+              </table >
+                
+              <br>
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-6">
+                           <div class="well well-lg">Thanks for Doing Business with us.</div>
+                      </div>
+                      <div class="col-md-6">
+                          <h3>Amount Due: $ 345.00</h3>
+                           <table class="table table-bordered table-striped col-md-10">
+                  <tr>
+                      <th>Sub Total</th>
+                      <td>$ 300.00</td>
+                  </tr>
+                  <tr>
+                      <th>Tax</th>
+                      <td>$ 300.00</td>
+                  </tr>
+                  <tr>
+                      <th>Discount</th>
+                      <td>$ 300.00</td>
+                  </tr>
+                  <tr>
+                      <th>Grand Total</th>
+                      <td>$ 300.00</td>
+                  </tr>
+                  <tr>
+                      <th>Recieved Amount</th>
+                      <td>$ 300.00</td>
+                  </tr>
+                  <tr>
+                      <th>Amount Due</th>
+                      <td>$ 345.00</td>
+                  </tr>
+                  
+                  
+              </table>
+                                                    
+                      </div>
+                      <a href="" class="btn btn-default">Print</a>
+                          <a href="" class="btn btn-success">Edit</a>
+                          <a href="" class="btn btn-primary">Email</a>
+                          <a href="" class="btn btn-primary">Generate PDF</a>
+                          <a href="" class="btn btn-primary">Add Payment</a>
+                          <a href="" class="btn btn-success">View Payment</a>
+                          <a href="" class="btn btn-warning">Cancel</a>
+                          <a href="" class="btn btn-danger">Delete</a>
+
+                  </div>
+              </div>
+             
+              
+             
+
+                
+                
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+      </div>
+      <!-- /.row (main row) -->
+
+    </section>
+    <!-- /.content -->
+  </div>
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php' ?>
